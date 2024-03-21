2157676073a9663803c95ca56998a478de143e7f
mahbub <mahbub.rahman@s2019.tu-chemnitz.de>
2024-03-21 23:24:26 +0100

    Read Blog Items

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -1,18 +1,35 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/', name: 'app_movie')]
+    private $movieRepository;
+
+    public function __construct(MovieRepository $movieRepository){
+        $this->movieRepository = $movieRepository;
+    }
+
+    #[Route('/', name: 'movies')]
     public function index(): Response
     {
-        return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+        $movies = $this->movieRepository->findAll();
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
         ]);
     }
+
+    #[Route('/movies/{id}', methods: ['GET'] , name: 'show')]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
+    }
+
 }
